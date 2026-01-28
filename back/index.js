afbcd5529729976468ac4f41fed7f0475e824a8e
gaurav-geeky <gauravmpreat@gmail.com>
2026-01-28 22:52:31 +0530

    restrict cors to frontend domain

--- back/index.js
+++ back/index.js
@@ -9,7 +9,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
 app.use("/home", (req, res) => {
